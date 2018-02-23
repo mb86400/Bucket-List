6d6840d5cfccf3a7e97d527be1cd1777d8075d95
M <m9rz86400@outlook.com>
2018-02-23 17:30:42 -0500

    Items Adding to list
    Items are being added to lists

--- Bucket-List/src/app/app.module.ts
+++ Bucket-List/src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import{ FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
@@ -17,6 +17,7 @@ import { AboutComponent } from './about/about.component';
   imports: [
     BrowserModule,
     AppRoutingModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Bucket-List/src/app/home/home.component.ts
+++ Bucket-List/src/app/home/home.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  itemCount: number;
+  btnText: string = 'Add an item'
+  goalText: string = 'My first goal'
+  goals = []
+
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { // this runs when the app loads
+    this.itemCount = this.goals.length;
+  }
+
+  addItem(){
+    this.goals.push(this.goalText);
+    this.goalText = ''; //clear after submited
+    this.itemCount = this.goals.length;
   }
 
 }
